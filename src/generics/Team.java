08afda18a2470d4f1c9de830a2e8644e15096918
bwielk <bwielk@gmail.com>
2018-02-24 21:38:06 +0100

    Players are added to the team

--- src/generics/Team.java
+++ src/generics/Team.java
@@ -9,7 +9,7 @@ public class Team {
 	private int won = 0;
 	private int lost = 0;
 	private int tied = 0;
-	private ArrayList<Player> mambers = new ArrayList<>();
+	private ArrayList<Player> members = new ArrayList<>();
 	
 	public Team(String name){
 		this.name = name;
@@ -17,5 +17,17 @@ public class Team {
 	
 	public String getName(){
 		return name;
+	}
+	
+	public boolean addPlayer(Player player){
+		if(!members.contains(player)){
+			members.add(player);
+			return true;
+		}
+		return false;
+	}
+	
+	public ArrayList getMembers(){
+		return members;
 	}
 }
